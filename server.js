e111f9d9fd36e6af787bfcfb85c1c14dece03b8c
ayush gupta <aerospace141@gmail.com>
2025-10-23 17:55:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const errorHandler = require('./src/middleware/errorHandler');
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({ origin: 'https://pharma-mwa.vercel.app' }));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
 
